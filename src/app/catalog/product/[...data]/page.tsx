58e70ca97fd461f73c2bdf500f016150eafc65dc
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-12-24 21:00:08 -0300

    feat: react server components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,17 +1,31 @@
+'use client'
+
 interface ProductProps {
   params: {
     data: string[]
   }
 }
 
+// Server Components => A gente não usa JavaScript no lado do cliente
+// Client Components => O JavaScript é enviado ao navegador (cliente)
+// Hydration => Tornar um HTML cru em um HTML interativo (Isso ocorre em todo client component)
+
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
+
+  console.log(params)
+
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
 
   return (
     <div>
       <h1>Product: {productId}</h1>
       <h1>Size: {size}</h1>
       <h1>Color: {color}</h1>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
