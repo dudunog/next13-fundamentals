64073ee895d039b4055232bc4c9918646632ed69
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-12-25 20:12:03 -0300

    feat: react suspense API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import { Suspense } from 'react'
+
+import { GithubProfile } from '@/components/github-profile'
+import { LongWaitComponent } from '@/components/long-wait-component'
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
-  const response = await fetch('https://api.github.com/users/dudunog')
-  const user = await response.json()
+  return (
+    <div>
+      <h1>Home</h1>
 
-  return <pre>{JSON.stringify(user, null, 2)}</pre>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure optio
+        quasi nam labore officiis vitae, quaerat impedit id fuga porro dicta
+        velit cum, voluptatibus nesciunt? Quam veritatis modi officiis quis.
+      </p>
+
+      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
+      <Suspense fallback={<p>Carregando GithubProfile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }
